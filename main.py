6477e527ef80e0032fa9ddf72fc701d0a6e2f078
Sviatoslav König <reveni324@gmail.com>
2024-07-21 17:20:02 +0300

    # add asynchronous offer processing and link streaming functionality
    - Integrate FastAPI for creating a web server with endpoints to process links and download files.
    - Implement asynchronous function `process_offer` for parsing offer elements from XML data.
    - Create `fetch_url_in_chunks` to stream and decode URL content in chunks asynchronously.
    - Develop `process_link_stream` to handle large XML files by dividing the work into asynchronous tasks.
    - Add endpoint `/process_link` to process input URLs and return downloadable CSV files.
    - Add endpoint `/download/data_files/{filename}` to enable file downloads from the server.
    - Improve HTML template and static files for better user interaction and feedback.
    - Setup URL routing and static file handling using FastAPI.

--- main.py
+++ main.py
@@ -1,79 +1,65 @@
 from urllib.parse import urlparse
-import requests
-import xml.etree.ElementTree as ET
-import csv
-import os
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-import spacy
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+import xml.etree.ElementTree as ET
 import re
+import csv
+import os
 import aiohttp
 import asyncio
+import aiofiles
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
-nlp = spacy.load("ru_core_news_sm")
 
 
 class LinkData(BaseModel):
   link_url: str
-  return_url: str = ""
   preset_id: str = ""
-
-
-def get_category_replacement(original_category_name, custom_categories):
-  vectorizer = TfidfVectorizer()
-  all_categories = [original_category_name] + custom_categories
-  tfidf_matrix = vectorizer.fit_transform(all_categories)
-  cosine_similarities = cosine_similarity(tfidf_matrix[0:1],
-                                          tfidf_matrix[1:]).flatten()
-  max_similarity_index = cosine_similarities.argmax()
-  if cosine_similarities[max_similarity_index] > 0:
-    return custom_categories[max_similarity_index]
-  else:
-    return original_category_name
-
-
-def load_custom_categories(filename):
-  custom_categories = []
-  with open(filename, mode='r', encoding='utf-8') as file:
-    reader = csv.reader(file)
-    custom_categories = [row[0] for row in reader]
-  return custom_categories
 
 
 def remove_unwanted_tags(description):
   if description:
     description = re.sub(r'<[^>]+>', '', description)
-    description = description.replace('•', '')
-    description = re.sub(r'\s*<br>\s*', '\n', description, flags=re.IGNORECASE)
-    description = re.sub(r'(\n\s*)+', '\n', description)
-    description = f'<p>{description.strip()}</p>'.replace('\n', '<br>')
   else:
     description = ''
   return description
 
 
-async def fetch_url(link_url):
+async def fetch_url_in_chunks(link_url, chunk_size=1024):
   async with aiohttp.ClientSession() as session:
     async with session.get(link_url) as response:
-      response.raise_for_status()
-      return await response.text()
+      buffer = b''
+      while True:
+        chunk = await response.content.read(chunk_size)
+        if not chunk:
+          if buffer:
+            yield buffer
+          break
+
+        buffer += chunk
+        try:
+          decoded_data = buffer.decode('utf-8')
+          yield decoded_data.encode('utf-8')
+          buffer = b''
+        except UnicodeDecodeError as e:
+          incomplete_bytes = e.start
+          yield buffer[:incomplete_bytes].decode('utf-8').encode('utf-8')
+          buffer = buffer[incomplete_bytes:]
 
 
-async def process_offer(offer_elem, categories, custom_categories):
+async def process_offer(offer_elem, build_category_path):
   offer_id = offer_elem.get('id', '0')
   offer_data = {'id': offer_id}
-  category_id = offer_elem.find('.//categoryId').text
-  original_category_name = categories.get(category_id, "Undefined")
-  offer_data['category_name'] = get_category_replacement(
-      original_category_name, custom_categories)
+
+  category_id_elem = offer_elem.find('.//categoryId')
+  category_id = category_id_elem.text if category_id_elem is not None else 'Undefined'
+  category_path = build_category_path(category_id)
+  offer_data['category_path'] = category_path
 
   for category_elem in offer_elem:
     if category_elem.tag not in ['picture', 'param']:
@@ -81,40 +67,33 @@ async def process_offer(offer_elem, categories, custom_categories):
       category_value = category_elem.text
       if category_value and category_value.replace('.', '', 1).isdigit():
         category_value = category_value.replace('.', ',')
-      offer_data[category_name] = category_value
+      offer_data[category_name] = category_value or ""
+
   picture_elems = offer_elem.findall('.//picture')
-  pictures = "///".join(
-      picture_elem.text
-      for picture_elem in picture_elems) if picture_elems else ""
+  pictures = "///".join([
+      picture_elem.text if picture_elem.text is not None else ""
+      for picture_elem in picture_elems
+  ]) if picture_elems else ""
+
   if pictures:
     offer_data['pictures'] = pictures
+
   param_elems = offer_elem.findall('.//param')
   params = {
-      param_elem.get('name'): param_elem.text
+      param_elem.get('name'): param_elem.text or ""
       for param_elem in param_elems
   } if param_elems else {}
   offer_data.update(params)
+
   if 'description' in offer_data and offer_data['description']:
     offer_data['description'] = remove_unwanted_tags(offer_data['description'])
+
   return offer_data
 
 
-async def process_link(link_url):
+async def process_link_stream(link_url, chunk_size=1024):
   try:
-    xml_data = await fetch_url(link_url)
-    root = ET.fromstring(xml_data)
-    custom_categories = load_custom_categories('categories.csv')
-
-    categories = {}
-    for category in root.findall('.//category'):
-      categories[category.get('id')] = category.text
-
-    tasks = [
-        process_offer(offer_elem, categories, custom_categories)
-        for offer_elem in root.findall('.//offer')
-    ]
-    data = await asyncio.gather(*tasks)
-
+    category_names = set()
     save_path = "data_files"
     os.makedirs(save_path, exist_ok=True)
     domain_name = urlparse(link_url).netloc.replace("www.", "")
@@ -122,20 +101,57 @@ async def process_link(link_url):
     unique_filename = f"{safe_filename}.csv"
     file_path = os.path.join(save_path, unique_filename)
 
-    category_names = set()
-    for row in data:
-      category_names.update(row.keys())
+    async with aiofiles.open(file_path, 'w', newline='',
+                             encoding='utf-8-sig') as file:
+      writer = None
 
-    with open(file_path, 'w', newline='', encoding='utf-8-sig') as file:
-      writer = csv.DictWriter(file,
-                              fieldnames=sorted(category_names),
-                              delimiter=';')
-      writer.writeheader()
-      writer.writerows(data)
-    return file_path
+      async for chunk in fetch_url_in_chunks(link_url, chunk_size):
+        chunk_data = chunk.decode('utf-8')
+        try:
+          root = ET.fromstring(chunk_data)
+        except ET.ParseError as e:
+          print(f"Ошибка при парсинге XML: {e}. Данные chunk: {chunk_data}")
+          continue
+
+        categories = {}
+        category_parents = {}
+        for category in root.findall('.//category'):
+          category_id = category.get('id')
+          parent_id = category.get('parentId')
+          categories[
+              category_id] = category.text if category.text is not None else 'Undefined'
+          if parent_id:
+            category_parents[category_id] = parent_id
+
+        def build_category_path(category_id):
+          path = []
+          while category_id:
+            path.append(categories.get(category_id, 'Undefined'))
+            category_id = category_parents.get(category_id)
+          return '///'.join(reversed(path))
+
+        tasks = [
+            process_offer(offer_elem, build_category_path)
+            for offer_elem in root.findall('.//offer')
+        ]
+        data = await asyncio.gather(*tasks)
+
+        if writer is None:
+          for row in data:
+            category_names.update(row.keys())
+          writer = csv.DictWriter(file,
+                                  fieldnames=sorted(category_names),
+                                  delimiter=';')
+          await file.write(
+              writer.writerow(dict((fn, fn) for fn in writer.fieldnames)))
+
+        for row in data:
+          await file.write(writer.writerow(row))
+
   except Exception as e:
-    print(f"An error occurred: {str(e)}")
+    print(f"Произошла ошибка: {str(e)}")
     return None
+  return file_path
 
 
 @app.get("/")
@@ -147,25 +163,26 @@ def read_index(request: Request):
 async def process_link_post(link_data: LinkData):
   link_url = link_data.link_url
   preset_id = link_data.preset_id
-  result = await process_link(link_url)
+  result = await process_link_stream(link_url)
   if result:
     downloaded_file_name = os.path.basename(result)
-    return {
-        "file_url":
-        f"https://soldata.replit.app/download/data_files/{downloaded_file_name}",
-        "preset_id": preset_id
-    }
+    file_url = f"https://solarxml.replit.app/download/data_files/{downloaded_file_name}"
+    print(f"Файл создан и доступен по URL: {file_url}")
+    return {"file_url": file_url, "preset_id": preset_id}
   else:
     raise HTTPException(status_code=500,
-                        detail="An error occurred while processing the link")
+                        detail="Во время обработки ссылки произошла ошибка")
 
 
 @app.get("/download/data_files/{filename}")
 async def download_csv(filename: str):
   file_path = os.path.join("data_files", filename)
+  print(f"Попытка загрузки файла: {file_path}")
   if os.path.isfile(file_path):
+    print("Файл найден, начинаю загрузку.")
     return FileResponse(path=file_path,
                         filename=filename,
                         media_type='application/octet-stream')
   else:
-    raise HTTPException(status_code=404, detail="File not found.")
+    print("Файл не найден.")
+    raise HTTPException(status_code=404, detail="Файл не найден.")
